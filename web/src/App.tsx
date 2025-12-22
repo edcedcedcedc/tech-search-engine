2aa1a898ed1f817eebce0cd2466d6760632ac612
Andro Ranogajec <ranogaet@gmail.com>
2025-12-22 23:47:59 +0200

    Rename routes to router, and get back to root /*

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import AppRoutes from "./routes/Routes";
+import AppRoutes from "./router/Router";
 import HeroSearch from "./components/HeroSearch";
 import { useState } from "react";
 
