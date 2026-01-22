e1c05341843bcb617cd1aaaa86e5fea16fdb7918
Andro Ranogajec <ranogaet@gmail.com>
2026-01-23 00:42:45 +0200

    Updates

--- server/products/management/commands/reset.py
+++ server/products/management/commands/reset.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
         "Use --reset-t-category to reset t_category for ArchivedProduct and ArchivedBrokenProduct."
     )
 
-    CRAWLER_DBS = ["xstore", "enter", "darwin", "stage", "default"]
+    CRAWLER_DBS = ["xstore", "enter", "darwin", "stage"]
     BATCH_SIZE = 500
 
     def add_arguments(self, parser):
